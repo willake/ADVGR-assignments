9517298bb2dc536f1cd40b6cbb71aa39c59b6834
Lincoln <willakelin@seekrtech.com>
2022-12-16 23:22:35 +0100

    update: add support for configing samplecount and depthlimity

--- path_trace_module.h
+++ path_trace_module.h
@@ -11,18 +11,18 @@ public:
 	float3 Trace(Ray& ray)
 	{
 		float3 result;
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < sampleCount; i++)
 		{
 			result += Sample(ray, 1);
 		}
 
-		result *= 2 * PI / 10;
+		result *= 2 * PI / sampleCount;
 		return result;
 	}
 
 	float3 Sample(Ray& ray, int depth)
 	{
-		if (depth > 5) return 0;
+		if (depth > depthLimit) return 0;
 		scene.FindNearest(ray);
 		if (ray.objIdx == -1) return 0; // or a fancy sky color
 		float3 I = ray.O + ray.t * ray.D;
@@ -81,59 +81,6 @@ public:
 
 		return PI * 2.0f * BRDF * Ei;
 	}
-	/*
-	float3 Sample(Ray& ray, int depth)
-	{
-		scene.FindNearest(ray);
-		if (ray.objIdx == -1) return 0; // or a fancy sky color
-		float3 I = ray.O + ray.t * ray.D;
-		float3 N = scene.GetNormal(ray.objIdx, I, ray.D);
-		Material material = scene.GetMaterial(ray.objIdx);
-		float3 albedo = scene.GetAlbedo(ray.objIdx, I); // very bad
-
-		if (material.isLight) return albedo * 0.3f;
-
-		//refraction of glass: 1.52 
-		float n1 = 1;
-		float n2 = 1.52f;
-		float n1DividedByn2 = n1 / n2;
-		float cosI = dot(N, -ray.D);
-		float k = 1 - (((n1 / n2) * (n1 / n2)) * (1 - (cosI * cosI)));
-
-		// glass
-		if (material.isGlass && depth < 5 && !(k < 0))
-		{
-			float ThetaI = acos(cosI);
-			float sinI = sin(ThetaI);
-			float3 refractDirection = (n1DividedByn2 * ray.D) + (N * ((n1DividedByn2 * cosI) - sqrt(k)));
-			float3 refraction = albedo * Trace(Ray(I + (refractDirection * 0.001f), refractDirection), depth + 1);
-
-			float cosT = sqrt(1 - (n1DividedByn2 * sinI));
-			float Rs = ((n1 * cosI) - (n2 * cosT)) / ((n1 * cosI) + (n2 * cosT));
-			float Rp = ((n1 * cosI) - (n2 * cosT)) / ((n1 * cosI) + (n2 * cosT));
-
-			float Fr = ((Rs * Rs) + (Rp * Rp)) / 2;
-			float Ft = 1 - Fr;
-
-			float3 reflectDirection = reflect(ray.D, N);
-			float3 reflection = albedo * Trace(Ray(I + reflectDirection * 0.001f, reflectDirection), depth + 1);
-			return (Fr * reflection) + (Ft * refraction);
-		}
-
-		if ((material.isMirror || (material.isGlass && k < 0)) && depth < 5)
-		{
-			float3 reflectDirection = reflect(ray.D, N);
-			return albedo * Trace(Ray(I + reflectDirection * 0.001f, reflectDirection), depth + 1);
-		}
-
-		float3 R = DiffuseReflection(N);
-
-		float3 BRDF = albedo / PI;
-
-		float3 Ei = Trace(Ray(I + R * 0.001f, R), depth + 1) * dot(R, N);
-
-		return 2.0f * PI * BRDF * Ei;
-	}*/
 
 	float3 DiffuseReflection(float3 N)
 	{
@@ -156,6 +103,8 @@ public:
 		return direction;
 	}
 
+	int depthLimit = 5;
+	int sampleCount = 10;
 	bool isInitialized = false;
 	Scene scene;
 };
